c59f887e3fa30ae9d43b8b5a076baeef3d83a68d
Batmetista <batista.as.pedro@gmail.com>
2024-02-23 17:07:06 +0000

    Add bool

--- VariousTypes/Program.cs
+++ VariousTypes/Program.cs
@@ -1,12 +1,23 @@
 ﻿using System;
 
 namespace VariousTypes
+int even = 8;
+int odd = 7;
+char copyrightSymbol = '\u00A9';
+char newLine = '\u000A';
+bool conditionno = false;
+bool conditionyes = true;
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine(even);
+            Console.WriteLine(odd);
+            Console.WriteLine(newLine);
+            Console.WriteLine(copyrightSymbol);
+            Console.WriteLine(conditionno);
+            Console.WriteLine(conditionyes);
         }
     }
 }
