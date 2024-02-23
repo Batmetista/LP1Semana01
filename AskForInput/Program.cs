8709a2ea357ffc3cf784770d36e823da4ce5b295
Batmetista <batista.as.pedro@gmail.com>
2024-02-23 17:32:46 +0000

    Add sum

---
+++ AskForInput/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace AskForInput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insira um número inteiro.");
+            string str = Console.ReadLine();
+            int str = Convert.ToInt32(Console.ReadLine(););
+            Console.WriteLine("Insira um número real.")
+            string aa = Console.ReadLine();
+            float aa = Convert.ToSingle(Console.ReadLine());
+            sum = aa + str
+            Console.WriteLine8(sum)
+        }
+    }
+}
