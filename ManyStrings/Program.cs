1a08bab81283f793f733b9bf677944f42ca1ee4d
Batmetista <batista.as.pedro@gmail.com>
2024-02-23 17:19:00 +0000

    Add conc, inter

--- ManyStrings/Program.cs
+++ ManyStrings/Program.cs
@@ -1,13 +1,18 @@
 ﻿using System;
 string test1 = "'\u00A9' This is a symbol."
 string test2 = "'\u000A' This is another symbol."
+string conc = "Uma "+ "string " + "concatenada";
+string inter = $"{x} mais {y} e' igual a {x + y}";
 namespace ManyStrings
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine(test1);
+            Console.WriteLine(test2);
+            Console.WriteLine(conc);
+            Console.WriteLine(inter);
         }
     }
 }
