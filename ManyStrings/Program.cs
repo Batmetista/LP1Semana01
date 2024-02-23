54663a717886d667e5904043ee11211f57555162
Batmetista <batista.as.pedro@gmail.com>
2024-02-23 17:14:12 +0000

    Add strings

---
+++ ManyStrings/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+string test1 = "'\u00A9' This is a symbol."
+string test2 = "'\u000A' This is another symbol."
+namespace ManyStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
