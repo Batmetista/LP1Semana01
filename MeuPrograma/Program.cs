179afdd293abd8852991d6631cfab09bb4b748d9
Batmetista <batista.as.pedro@gmail.com>
2024-02-23 16:19:00 +0000

    Add bye world

--- MeuPrograma/Program.cs
+++ MeuPrograma/Program.cs
@@ -7,7 +7,6 @@ namespace MeuPrograma
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
-            Console.WriteLine("Bye, World!");
         }
     }
 }
